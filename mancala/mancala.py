c2ec7a285d34efbbfc41e1b03ade7b86280e7111
Devin <d3v1ngaines@gmail.com>
2023-10-05 12:11:42 -0400

    2x6 Grid

--- mancala/mancala.py
+++ mancala/mancala.py
@@ -2,12 +2,18 @@ import tkinter as tk
 import mechanics
 
 
-root = tk.Tk()
-root.title = "Mancala"
+window  = tk.Tk()
+window.title = "Mancala"
+
+#2x6
+buttons = [[4,4,4], [4,4,4], [4,4,4]]
+for row in range(2): #Creates a row of three buttons and they get added to the buttons list
+    for col in range(6):
+        buttons[row][col] = tk.Button(window, text="", font=("Arial", 75))
+        buttons[row][col].grid(row=row, column=col)
 
 
 
 
 
-
-root.mainloop()
+window.mainloop()
