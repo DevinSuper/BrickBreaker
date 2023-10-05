76d855e5de2fe83a409f7846bc9ad15e0780a49f
Croculus <awildsnappyboi@gmail.com>
2023-10-05 11:30:14 -0400

    mancala

---
+++ mancala/mancala.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+
+
+root = tk.Tk()
+root.title = "Mancala"
+
+
+
+class Board():
+    def __init__(self):
+        board : [int]
+        score1 : int
+        score2 : int
+        turn : bool
+
+
+
+
+root.mainloop()
